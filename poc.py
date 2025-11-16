a680eb7a0a3b3f407fddcefe589fb8992b2195e4
r3vskd <dymethylcadmium@protonmail.ch>
2025-11-16 15:11:00 +0000

    poc: increase default UDP bufsize to 4096

--- poc.py
+++ poc.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     parser.add_argument('--qtype', type=str, default='A', help='DNS query type')
     parser.add_argument('--edns_payload', type=int, default=0, help='EDNS UDP payload size')
     parser.add_argument('--dnssec_do', action='store_true', help='Enable DNSSEC DO bit')\n    parser.add_argument('--edns_nsid', action='store_true', help='Request NSID via EDNS OPT')\n    parser.add_argument('--no_rd', action='store_true', help='Disable recursion (RD=0)')
-    parser.add_argument('--measure', action='store_true', help='Show query/response sizes and ratio')\n    parser.add_argument('--bufsize', type=int, default=2048, help='Receive buffer size')
+    parser.add_argument('--measure', action='store_true', help='Show query/response sizes and ratio')\n    parser.add_argument('--bufsize', type=int, default=4096, help='Receive buffer size')
 
     args = parser.parse_args()
 
@@ -175,6 +175,7 @@ def send_dns_query_tcp(domain_name, dns_server_address, dns_server_port, verbose
         print(f"Query size: {len(query)} bytes")
         print(f"Response size: {len(data)} bytes")
         print(f"Ratio: {round(len(data)/len(query), 2)}x")
+
 
 
 
