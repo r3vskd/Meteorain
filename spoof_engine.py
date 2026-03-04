b8c02652760effbbf76a598e9e9ed43784d493ab
revskd <dymethylcadmium@protonmail.ch>
2026-03-04 10:39:04 -0600

    feat: add platform-aware _is_root() detection

--- spoof_engine.py
+++ spoof_engine.py
@@ -1,1 +1,11 @@
 """DNS amplification spoof engine -- Layer-3 IP spoofing via Scapy."""
+
+import os
+import sys
+
+
+def _is_root():
+    if sys.platform == 'win32':
+        import ctypes
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return os.getuid() == 0
