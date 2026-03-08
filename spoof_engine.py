13570c96ec0e064d25d96ec24580c91bb4af6d5e
revskd <dymethylcadmium@protonmail.ch>
2026-03-08 10:39:04 -0600

    feat: add QTYPE_MAP lookup table to spoof_engine

--- spoof_engine.py
+++ spoof_engine.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+QTYPE_MAP = {
+    'A': 1, 'AAAA': 28, 'ANY': 255, 'DNSKEY': 48, 'DS': 43,
+    'TXT': 16, 'NS': 2, 'CNAME': 5, 'SOA': 6, 'PTR': 12,
+    'MX': 15, 'SRV': 33, 'NAPTR': 35, 'CAA': 257,
+}
+
 
 def _is_root():
     if sys.platform == 'win32':
