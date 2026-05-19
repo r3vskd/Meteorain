7ab00cd16ea2890a1242083ada10cb630420f4ab
revskd <dymethylcadmium@protonmail.ch>
2026-05-19 10:39:04 -0600

    chore: add version tag to spoof_engine header

--- spoof_engine.py
+++ spoof_engine.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2026 r3vskd
+# Version: 2.0.0
 """DNS amplification spoof engine -- Layer-3 IP spoofing via Scapy.
 
 WARNING: For authorized penetration testing only. Using this tool against
